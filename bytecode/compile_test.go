666c9402d0b0138326772fe43ff1921fbc929649
Martin Jonson <marjo886@student.liu.se>
2025-02-12 10:10:53 +0100

    compile test

---
+++ bytecode/compile_test.go
@@ -0,0 +1,33 @@
+package bytecode
+
+import "testing"
+
+func TestCompileSimpleProgram(t *testing.T) {
+	program := "+++[-]"
+	bytes := CompileProgram(program)
+	expectedBytes := []byte{9, 3, 14}
+
+	if !isEqual(bytes, expectedBytes) {
+		t.Fatalf("Compiled byte code should be %d but was %d", expectedBytes, bytes)
+	}
+}
+
+func TestCompileEveryInstructionButJump(t *testing.T) {
+	program := "><+-.,+++--->>><<<[-]>[-]abcde"
+	bytes := CompileProgram(program)
+	expectedBytes := []byte{1, 2, 3, 4, 5, 6, 9, 3, 10, 3, 11, 3, 12, 3, 13, 14}
+
+	if !isEqual(bytes, expectedBytes) {
+		t.Fatalf("Compiled byte code should be %d but was %d", expectedBytes, bytes)
+	}
+}
+
+func TestCompileJumps(t *testing.T) {
+	program := "[+]"
+	bytes := CompileProgram(program)
+	expectedBytes := []byte{7, 0, 0, 0, 10, 3, 8, 0, 0, 0, 4}
+
+	if !isEqual(bytes, expectedBytes) {
+		t.Fatalf("Compiled byte code should be %d but was %d", expectedBytes, bytes)
+	}
+}
