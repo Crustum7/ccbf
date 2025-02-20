d4398040fb9bce9c8dc82fbe3e8b0f83329b9292
Martin Jonson <marjo886@student.liu.se>
2025-02-20 21:02:36 +0100

    program counter test

---
+++ instructions/programCounter_test.go
@@ -0,0 +1,45 @@
+package instructions_test
+
+import (
+	"testing"
+
+	"martinjonson.com/ccbf/instructions"
+)
+
+func TestProgramCounterInitialValue(t *testing.T) {
+	pc := instructions.InitProgramCounter()
+
+	if pc.Get() != 0 {
+		t.Fatalf("Program counter should start at 0 and not %d", pc.Get())
+	}
+}
+
+func TestProgramCounterIncrement(t *testing.T) {
+	pc := instructions.InitProgramCounter()
+
+	pc.Increment()
+
+	if pc.Get() != 1 {
+		t.Fatalf("Program counter should be 1 and not %d", pc.Get())
+	}
+}
+
+func TestProgramCounterIncrementWith(t *testing.T) {
+	pc := instructions.InitProgramCounter()
+
+	pc.IncrementWith(5)
+
+	if pc.Get() != 5 {
+		t.Fatalf("Program counter should be 5 and not %d", pc.Get())
+	}
+}
+
+func TestProgramCounterSet(t *testing.T) {
+	pc := instructions.InitProgramCounter()
+
+	pc.Set(42)
+
+	if pc.Get() != 42 {
+		t.Fatalf("Program counter should be 42 and not %d", pc.Get())
+	}
+}
